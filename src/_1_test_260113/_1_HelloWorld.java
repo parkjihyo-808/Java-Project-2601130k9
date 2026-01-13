892a5f04a18745f896e68ea59d403a6e3f5e7013
lsy3709 <lsy3709@naver.com>
2026-01-13 11:21:55 +0900

    3교시, 코드 정리2

---
+++ src/_1_test_260113/_1_HelloWorld.java
@@ -0,0 +1,9 @@
+package _1_test_260113;
+
+public class _1_HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("깃 연동 테스트 확인");
+        System.out.println("본인 깃허브 포크한 레포진토리 " +
+                "싱크포크 -> 인텔리제이 창 -> 패치 -> 업데이트");
+    }
+}
